7c3d982ad3fe31d3c54767205eef806a40bce34e
Joerg Delker <jd_github@onix.de>
2019-12-13 10:40:20 +0100

    replaced static VERSION with dynamic property template

--- Procyon.Core/src/main/java/com/strobel/Procyon.java
+++ Procyon.Core/src/main/java/com/strobel/Procyon.java
@@ -1,7 +1,7 @@
 /*
  * Framework.java
  *
- * Copyright (c) 2015 Mike Strobel
+ * Copyright (c) 2015 Mike Strobel, 2019 Joerg Delker
  *
  * This source code is subject to terms and conditions of the Apache License, Version 2.0.
  * A copy of the license can be found in the License.html file at the root of this distribution.
@@ -10,13 +10,31 @@
  *
  * You must not remove this notice, or any other, from this software.
  */
-
 package com.strobel;
 
-public final class Procyon {
-    private static final String VERSION = "1.0-SNAPSHOT";
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    public static String version() {
-        return VERSION;
+public final class Procyon {
+
+  private final static String PROP_RESOURCE = "procyon.properties";
+  private static String _version;
+
+  public static String version() {
+    if (_version == null) {
+      ClassLoader loader = Thread.currentThread().getContextClassLoader();
+      Properties props = new Properties();
+      try (InputStream resourceStream = loader.getResourceAsStream(PROP_RESOURCE)) {
+        props.load(resourceStream);
+      } catch (IOException ex) {
+        Logger.getLogger(Procyon.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      _version = props.getProperty("procyon.version", "undefined");
     }
+
+    return _version;
+  }
 }
